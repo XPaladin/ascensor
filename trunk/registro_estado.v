8bb6707d97378fe111d0185e0dd684ad035cb881
gjorquera <gjorquera@1340b400-9ae2-11dd-92ee-791284ec580f>
2008-10-16 02:00:29 +0000

    xx

---
+++ trunk/registro_estado.v
@@ -0,0 +1,12 @@
+/**
+ * Registro para almacenar el estado del ascensor.
+ *
+ * @param in:D    La entrada de la informacion.
+ * @param in:clk  La entrada del reloj.
+ * @param out:Q   La salida de la informacion almacenada.
+ */
+module REGISTRO_ESTADO (D, clk, Q);
+	input [3:0] D;
+	input clk;
+	output [3:0] Q;
+endmodule
---
+++ trunk/registro_solicitudes.v
@@ -0,0 +1,12 @@
+/**
+ * Registro para almacenar el estado de las solicitudes.
+ *
+ * @param in:D    La entrada de la informacion.
+ * @param in:clk  La entrada del reloj.
+ * @param out:Q   La salida de la informacion almacenada.
+ */
+module REGISTRO_SOLICITUDES (D, clk, Q);
+	input [9:0] D;
+	input clk;
+	output [9:0] Q;
+endmodule

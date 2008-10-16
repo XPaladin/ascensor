213c904255f3f8a9135735abebf6282abc2df677
gjorquera <gjorquera@1340b400-9ae2-11dd-92ee-791284ec580f>
2008-10-16 02:00:29 +0000

    xx

--- compiler/compiler.tcl
+++ compiler/compiler.tcl
@@ -4,7 +4,7 @@ set target_library [list lsi_10k.db]
 set link_library [concat $target_library "*"]
 
 # Analyze - Elaborate
-analyze -f verilog {../top.v ../registrador.v ../algoritmo.v ../interfaz_salida.v ../control_puertas.v ../timeout.v}
+analyze -f verilog {../top.v ../registrador.v ../registro_solicitudes ../registro_estado ../algoritmo.v ../interfaz_salida.v ../control_puertas.v ../timeout.v}
 elaborate TOP
 
 # Check design
